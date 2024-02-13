d1468c6f1ba9557ece9b59185d534fbfdaba71ff
w68264Ligezka <luk.lig@wp.pl>
2024-02-13 19:30:44 +0100

    0.1 Przygotowanie pierszych polecen

---
+++ w68264/Lekarz.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+
+namespace w68264
+{
+    class Lekarz
+    {
+
+
+
+
+
+
+
+        public void wysDanePac(string pesel)
+        {
+            string connectionString = "Data Source=(localdb)\\Local;Database=w68264_Projekt;Integrated Security=True";
+
+
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string selectDataQuery = "select * from Pacjenci where pesel = @pesel";
+
+                using (SqlCommand selectDataCommand = new SqlCommand(selectDataQuery, connection))
+                {
+                    selectDataCommand.Parameters.AddWithValue("@Pesel", pesel);
+                    using (SqlDataReader reader = selectDataCommand.ExecuteReader())
+                    {
+                        Console.WriteLine("\nPacjent:");
+                        while (reader.Read())
+                        {
+
+                            string firstName = reader.IsDBNull(reader.GetOrdinal("Imie")) ? string.Empty : reader.GetString(reader.GetOrdinal("Imie"));
+                            string lastName = reader.IsDBNull(reader.GetOrdinal("Nazwisko")) ? string.Empty : reader.GetString(reader.GetOrdinal("Nazwisko"));
+                            string pes = reader.IsDBNull(reader.GetOrdinal("Pesel")) ? string.Empty : reader.GetString(reader.GetOrdinal("Pesel"));
+                            string lek = reader.IsDBNull(reader.GetOrdinal("Przyjmowane_Leki")) ? string.Empty : reader.GetString(reader.GetOrdinal("Przyjmowane_Leki"));
+                            string historia = reader.IsDBNull(reader.GetOrdinal("Historia_leczenia")) ? string.Empty : reader.GetString(reader.GetOrdinal("Historia_leczenia"));
+
+                            Console.WriteLine($"Imię: {firstName}\n Nazwisko: {lastName}\n Pesel: {pes}\nPrzyjmowane Leki:{lek}\n");
+                            Console.WriteLine($"Historia Leczenia:{historia}");
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+
+
+        }
+        public void zlecBadanie(string pesel,string nazwaBadania,string data)//'2024-02-20'
+        {
+            string connectionString = "Data Source=(localdb)\\Local;Database=w68264_Projekt;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string insertDataQuery = $"INSERT INTO Wykonane_Badanie (Nazwa, Pacjent, Data) VALUES ('{nazwaBadania}','{pesel}','{data}')";
+
+                   using (SqlCommand insertDataCommand = new SqlCommand(insertDataQuery, connection))
+                   {
+                        try
+                        {
+                            insertDataCommand.ExecuteNonQuery();
+                            Console.WriteLine($"Dodano badanie: {nazwaBadania},    {pesel},     {data}");
+                        }
+                            catch (Exception ex)
+                        {
+                            Console.WriteLine($"Błąd przy dodawaniu danych: {ex.Message}");
+                        }
+                   }
+                        
+                    
+            }
+                
+
+        }
+
+        public void wysBadaniaPac(string pesel)
+        {
+            string connectionString = "Data Source=(localdb)\\Local;Database=w68264_Projekt;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string selectDataQuery = "select Nazwa,Cast(Data as date) as Termin,Wynik from Wykonane_Badanie where Pacjent = @pesel";
+
+                using (SqlCommand selectDataCommand = new SqlCommand(selectDataQuery, connection))
+                {
+                    selectDataCommand.Parameters.AddWithValue("@pesel", pesel);
+                    using (SqlDataReader reader = selectDataCommand.ExecuteReader())
+                    {
+                        Console.WriteLine("\nBadania Pacjenta:");
+                        while (reader.Read())
+                        {
+
+                            string nazwa = reader.IsDBNull(reader.GetOrdinal("Nazwa")) ? "Null" : reader.GetString(reader.GetOrdinal("Nazwa"));
+                            DateTime termin = reader.GetDateTime(reader.GetOrdinal("Termin"));
+                            string wynik = reader.IsDBNull(reader.GetOrdinal("Wynik")) ? "Null" : reader.GetString(reader.GetOrdinal("Wynik"));
+                            
+
+                            Console.WriteLine($"Nazwa Badania: {nazwa}\n Data: {termin}\n Wynik: {wynik}");
+                            Console.WriteLine();
+                        }
+                    }
+                }
+            }
+
+
+        }
+
+
+        public void zaplanujOperacje(string pesel, string nazwaOperacji)//'2024-02-20'
+        {
+            string connectionString = "Data Source=(localdb)\\Local;Database=w68264_Projekt;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string insertDataQuery = $"INSERT INTO Wykonane_Operacjie(Nazwa, Pacjent) VALUES ('{nazwaOperacji}','{pesel}')";
+
+                using (SqlCommand insertDataCommand = new SqlCommand(insertDataQuery, connection))
+                {
+                    try
+                    {
+                        insertDataCommand.ExecuteNonQuery();
+                        Console.WriteLine($"Dodano badanie: {nazwaOperacji},    {pesel}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Błąd przy dodawaniu danych: {ex.Message}");
+                    }
+                }
+
+
+            }
+
+
+        }
+
+
+
+        public void raportOperacja(int idOperacji, int Personel, string wynik)//'2024-02-20'
+        {
+            string connectionString = "Data Source=(localdb)\\Local;Database=w68264_Projekt;Integrated Security=True";
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+
+                string insertDataQuery = $"UPDATE Wykonane_Operacjie SET Personel = {Personel}, Wynik = '{wynik}' WHERE IdOperacji = {idOperacji}";
+
+                using (SqlCommand insertDataCommand = new SqlCommand(insertDataQuery, connection))
+                {
+                    try
+                    {
+                        insertDataCommand.ExecuteNonQuery();
+                        Console.WriteLine($"Wprowadzono wynik operacji: {Personel},   {wynik},    {idOperacji}");
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"Błąd przy dodawaniu danych: {ex.Message}");
+                    }
+                }
+
+
+            }
+
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- w68264/Program.cs
+++ w68264/Program.cs
@@ -1,2 +1,61 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Data.SqlClient;
+using System;
+using System.Security.Cryptography.X509Certificates;
+using w68264;
+
+//bool exit = false;
+
+//while (!exit)
+//{
+//    Console.WriteLine("Kto:");
+//    Console.WriteLine("1. Lekarz");
+//    Console.WriteLine("2. Pielengniarka");
+//    Console.WriteLine("3. Pacjent");
+//    Console.WriteLine("4. Wyjście");
+
+//    Console.Write("Twój wybór: ");
+//    string choice = Console.ReadLine();
+
+//    switch (choice)
+//    {
+//        case "1":
+//            Console.WriteLine("Wybrano opcję 1.");
+//            // Tutaj dodaj kod dla opcji 1
+//            break;
+//        case "2":
+//            Console.WriteLine("Wybrano opcję 2.");
+//            // Tutaj dodaj kod dla opcji 2
+//            break;
+//        case "3":
+//            Console.WriteLine("Wybrano opcję 3.");
+//            // Tutaj dodaj kod dla opcji 3
+//            break;
+//        case "4":
+//            Console.WriteLine("Wybrano wyjście. Program zostanie zakończony.");
+//            exit = true;
+//            break;
+//        default:
+//            Console.WriteLine("Nieprawidłowy wybór. Spróbuj ponownie.");
+//            break;
+//    }
+
+
+Lekarz Piotr = new Lekarz();
+
+
+//Piotr.wysDanePac("03988468897");
+//Piotr.zlecBadanie("03988468897", "krew", "2024-03-20 12:30:00");
+//Piotr.wysBadaniaPac("03988468897");
+//Piotr.zaplanujOperacje("03988468897", "gasto");
+//Piotr.raportOperacja(4, 2, "Negatywny");
+
+
+
+
+
+
+
+
+
+
+
